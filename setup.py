a5cd3b70ae58c45c7714471b2f0ed7853ca7fe31
Charlie Denton <charlie@meshy.co.uk>
2012-11-15 09:42:57 +0000

    Fix ImportError in setup.py.

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
-import app_build
+import open_app
 
 
 setup(
     name='django-app-build',
     packages=find_packages(),
     include_package_data=True,
-    version=app_build.__version__,
+    version=open_app.__version__,
     description='',
     long_description=open('README.rst').read(),
-    author=app_build.__author__,
+    author=open_app.__author__,
     author_email='admin@incuna.com',
     url='http://incuna.com/',
     install_requires=[],
     zip_safe=False,
 )
-
